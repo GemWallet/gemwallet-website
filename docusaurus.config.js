42656e35f9fd89e1906a775f31e5ae4bd1f9670d
Florian Bouron <bouron.florian@yahoo.fr>
2022-02-21 21:25:38 +0100

    Add configuration for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'GemWallet', // Usually your GitHub org/user name.
   projectName: 'gemwallet-website', // Usually your repo name.
-
+  trailingSlash: false,
   presets: [
     [
       'classic',
