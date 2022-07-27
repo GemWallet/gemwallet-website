26ff0d5c7a9a652703dc7f011f75f5f145acd333
Florian Bouron <bouron.florian@yahoo.fr>
2022-07-27 20:36:49 +0200

    Add TailwindCSS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,17 @@ const config = {
         trackingID: "G-6K1MGTVRGX",
       },
     ],
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
   ],
 
   themeConfig:
