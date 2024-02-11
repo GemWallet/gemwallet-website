b082b66f12215168d823982f7aebc476089107f9
Florian Bouron <bouron.florian@yahoo.fr>
2024-02-11 16:52:58 +0100

    Add more additional languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -159,8 +160,9 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} GemWallet`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        additionalLanguages: ['bash', 'js-extras', 'tsx', 'html'],
       },
       colorMode: {
         defaultMode: "dark",
